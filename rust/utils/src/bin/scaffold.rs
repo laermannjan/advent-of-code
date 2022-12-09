abb4cf7dca08e5101d29751f8c2ef8ee17ff0c51
Jan Laermann <laermannjan@gmail.com>
2022-12-09 15:07:43 +0100

    feat(rust): updated day_template to include DAY, YEAR tokens

--- rust/utils/src/bin/scaffold.rs
+++ rust/utils/src/bin/scaffold.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs::OpenOptions, io::Write, process::Command};
+use std::{fs::OpenOptions, io::Write, process::Command};
 
 use clap::Parser;
 
@@ -11,29 +11,37 @@ pub struct Scaffold {
 }
 
 const TEMPLATE: &str = r###"utils = { path = \"../utils\" }"###;
-const MODULE_TEMPLATE: &str = r###"pub fn part_one(input: &str) -> Option<u32> {
+const DAY_TEMPLATE: &str = r###"type Input = Vec<i32>;
+
+pub fn parse_input(input: &str) -> Input {
+    input.lines().map(|s| s.parse::<i32>().unwrap_or(0)).collect()
+}
+pub fn part_one(input: Input) -> Option<i32> {
     None
 }
-pub fn part_two(input: &str) -> Option<u32> {
+
+pub fn part_two(input: Input) -> Option<i32> {
     None
 }
-fn main() {
-    let input = &utils:read_file("inputs", DAY);
-    utils::solve!(1, part_one, input);
-    utils::solve!(2, part_two, input);
-}
+
+utils::main!(YEAR, DAY);
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_part_one() {
-        let input = utils:read_file("examples", DAY);
-        assert_eq!(part_one(&input), None);
+        let input = utils::get_test_input(YEAR, DAY);
+        let parsed_input = parse_input(&input);
+        assert_eq!(part_one(parsed_input), Some(1337));
     }
+
     #[test]
     fn test_part_two() {
-        let input = utils::read_file("examples", DAY);
-        assert_eq!(part_two(&input), None);
+        let input = utils::get_test_input(YEAR, DAY);
+        let parsed_input = parse_input(&input);
+        assert_eq!(part_two(parsed_input), Some(1337));
     }
 }
 "###;
@@ -76,14 +84,32 @@ fn create_file(path: &str, content: &str) -> Result<(), std::io::Error> {
 }
 fn create_day(year: u32, day: u8) -> Result<(), std::io::Error> {
     let crate_path_prefix = format!("{}/../", env!("CARGO_MANIFEST_DIR"));
-    create_file(
-        &format!("{}aoc-{}/src/bin/{:02}.rs", crate_path_prefix, year, day),
-        MODULE_TEMPLATE,
-    )?;
+    let path = format!("{}aoc-{}/src/bin/{:02}.rs", crate_path_prefix, year, day);
+    let template = DAY_TEMPLATE
+        .replace("YEAR", &format!("{}", year))
+        .replace("DAY", &format!("{}", day));
+
+    if std::path::Path::new(&path).exists() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::AlreadyExists,
+            "File already exists",
+        ));
+    }
+
+    create_file(&path, &template)?;
     Ok(())
 }
 
 fn scaffold_year(year: u32) -> Result<(), std::io::Error> {
+    let crate_path = format!("{}/../aoc-{}", env!("CARGO_MANIFEST_DIR"), year);
+
+    if std::path::Path::new(&crate_path).exists() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::AlreadyExists,
+            "Crate already exists",
+        ));
+    }
+
     create_crate(year)?;
     Ok(())
 }
@@ -95,11 +121,41 @@ fn scaffold_day(year: u32, day: u8) -> Result<(), std::io::Error> {
 
 fn main() {
     let scaffold = Scaffold::parse();
-    println!("{:?}", scaffold);
 
-    if let Err(e) = scaffold_year(scaffold.year) {
-        eprintln!("Error: {}", e);
-    } else if let Err(e) = scaffold_day(scaffold.year, scaffold.day) {
-        eprintln!("Error: {}", e);
+    match scaffold_year(scaffold.year) {
+        Ok(_) => println!(
+            "Created create for {} => ./aoc-{}",
+            scaffold.year, scaffold.year
+        ),
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::AlreadyExists => {
+                println!(
+                    "Crate for {} already exists, skipping creation",
+                    scaffold.year
+                );
+            }
+            _ => {
+                println!("Error: {}", e);
+                std::process::exit(1);
+            }
+        },
+    }
+    match scaffold_day(scaffold.year, scaffold.day) {
+        Ok(_) => println!(
+            "Created binary for day {} => ./aoc-{}/src/bin/{:02}.rs",
+            scaffold.day, scaffold.year, scaffold.day
+        ),
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::AlreadyExists => {
+                println!(
+                    "File for Day {} binary already exists, skipping creation",
+                    scaffold.day
+                );
+            }
+            _ => {
+                println!("Error: {}", e);
+                std::process::exit(1);
+            }
+        },
     }
 }
