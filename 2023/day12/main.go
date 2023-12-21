21793b24229ffb600a0a2a53a4e890daa234b3fb
Jan Laermann <laermannjan@gmail.com>
2023-12-21 02:59:09 +0100

    go: 2023/12

--- 2023/day12/main.go
+++ 2023/day12/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoc-go/utils"
+	"fmt"
 	"log"
 	"strings"
 )
@@ -21,6 +22,48 @@ func combinations(cfg string) (combos []string) {
 		}
 	}
 	return
+}
+
+type key struct {
+	cfg    string
+	groups string
+}
+
+func counts(cfg string, groups []int, cache map[key]int) int {
+	if cfg == "" {
+		if len(groups) == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+	if len(groups) == 0 {
+		if strings.Contains(cfg, "#") {
+			return 0
+		} else {
+			return 1
+		}
+	}
+	str_groups := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(groups)), ","), "[]")
+	if mem_res, ok := cache[key{cfg: cfg, groups: str_groups}]; ok {
+		return mem_res
+	}
+
+	res := 0
+	if cfg[0] == '.' || cfg[0] == '?' {
+		res += counts(cfg[1:], groups, cache)
+	}
+	if cfg[0] == '#' || cfg[0] == '?' {
+		if groups[0] <= len(cfg) && !strings.Contains(cfg[:groups[0]], ".") && (groups[0] == len(cfg) || cfg[groups[0]] != '#') {
+			if groups[0]+1 <= len(cfg)-1 {
+				res += counts(cfg[groups[0]+1:], groups[1:], cache)
+			} else {
+				res += counts("", groups[1:], cache)
+			}
+		}
+	}
+	cache[key{cfg: cfg, groups: str_groups}] = res
+	return res
 }
 
 func part1(input utils.Input) (answer interface{}) {
@@ -50,7 +93,7 @@ func part1(input utils.Input) (answer interface{}) {
 						continue combo_loop
 					}
 				}
-				// log.Println("combo:", combo, combo_groups)
+				log.Println("combo:", combo, combo_groups)
 				total_combos++
 			}
 		}
@@ -60,7 +103,29 @@ func part1(input utils.Input) (answer interface{}) {
 }
 
 func part2(input utils.Input) (answer interface{}) {
-	return
+	total_combos := 0
+	cache := make(map[key]int)
+	for _, line := range input.LineSlice() {
+		parts := strings.Split(line, " ")
+		config := parts[0]
+		var groups []int
+		for _, g := range strings.Split(parts[1], ",") {
+			groups = append(groups, utils.Atoi(g))
+		}
+
+		log.Println()
+		log.Println(config, groups)
+
+		config = config + "?" + config + "?" + config + "?" + config + "?" + config
+		groups = append(groups, append(groups, append(groups, append(groups, groups...)...)...)...)
+
+		log.Println(config, groups)
+
+		combos := counts(config, groups, cache)
+		log.Println("combos:", combos)
+		total_combos += combos
+	}
+	return total_combos
 }
 
 func main() {
