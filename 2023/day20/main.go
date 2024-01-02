a68fe4b7b288b516a7c33c6d7bf1a5478de769c0
Jan Laermann <laermannjan@gmail.com>
2024-01-02 10:21:24 +0100

    go: 2023/20

--- 2023/day20/main.go
+++ 2023/day20/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc-go/utils"
 	"log"
+	"slices"
 	"strings"
 )
 
@@ -181,8 +182,99 @@ func part1(input utils.Input) (answer interface{}) {
 	return low_count * high_count
 }
 
+// could only solve this with some assumptions without basis
+// the target "rx" has only one input, which is a Conjunction
+// a conjuction sends a low pulse if all of its inputs have been sending a high pulse
+// so I assume that its inputs send high pulses in intervals and we need to
+// find the earliest button press where all these intervals coincide
 func part2(input utils.Input) (answer interface{}) {
-	return
+	modules := map[string]Module{}
+	feed := ""
+	cycles := map[string]int{}
+
+	for line := range input.Lines() {
+		con := strings.Split(line, " -> ")
+		outputs := strings.Split(con[1], ", ")
+
+		var module string
+		if con[0] == "broadcaster" {
+			module = con[0]
+			modules[module] = &Broadcaster{outputs: outputs}
+
+		} else {
+			con_runes := []rune(con[0])
+			module = string(con_runes[1:])
+			switch con_runes[0] {
+			case '%':
+				modules[module] = &FlipFlop{state: low, outputs: outputs}
+			case '&':
+				modules[module] = &Conjunction{states: map[string]Pulse{}, outputs: outputs}
+			}
+		}
+		if slices.Contains(outputs, "rx") {
+			feed = module
+		}
+
+	}
+
+	for k, v := range modules {
+		for _, out := range v.getOutputs() {
+			switch modules[out].(type) {
+			case *Conjunction:
+				modules[out].(*Conjunction).states[k] = low
+			}
+		}
+	}
+
+	for k, v := range modules {
+		log.Printf("%v: %#v", k, v)
+	}
+
+	rx_count := 0
+
+	i := 0
+	for {
+		i++
+		// log.Println("i", i)
+
+		init := Transmission{
+			from:  "button", // maybe this causes troubles with conjunctions
+			to:    "broadcaster",
+			pulse: low,
+		}
+
+		stack := []Transmission{init}
+
+		// log.Println("stack:", stack)
+		for len(stack) > 0 {
+			t := stack[0]
+
+			if t.to == feed && t.pulse == high {
+				cycles[t.from] = i - cycles[t.from]
+			}
+
+			// log.Printf("%v -%v-> %v", t.from, t.pulse, t.to)
+			stack = stack[1:]
+
+			if receiver, ok := modules[t.to]; ok {
+				outputs := receiver.output(t)
+				// log.Println("adding outputs", outputs)
+				stack = append(stack, outputs...)
+
+			}
+		}
+		if rx_count == 1 || i > 10000 {
+			break
+		}
+	}
+
+	log.Println("cycles", cycles, "rx", rx_count)
+
+	cycle_lengths := []int{}
+	for _, cycle := range cycles {
+		cycle_lengths = append(cycle_lengths, cycle)
+	}
+	return utils.LCM(cycle_lengths...)
 }
 
 func main() {
