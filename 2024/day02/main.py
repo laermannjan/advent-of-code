ebcd954b175915abbd5a5b967e6e963b08bdd7b1
Jan Laermann <git@flabber.mozmail.com>
2024-12-03 00:54:00 +0100

    python: 2024/2

--- 2024/day02/main.py
+++ 2024/day02/main.py
@@ -4,12 +4,13 @@ from utils.execute import Day
 from utils.input import Input
 
 
-def is_safe(report: list[int]) -> bool:
+def has_problem(report: list[int]) -> int | None:
+    """Returns None if report is safe. Index of the left element of the pair that resulted in a problem otherwise."""
     increasing = decreasing = True
-    for prev, next in pairwise(report):
+    for i, (prev, next) in enumerate(pairwise(report)):
         if not (1 <= abs(next - prev) <= 3):
             print("step too large")
-            return False
+            return i
 
         if next < prev:
             increasing = False
@@ -17,17 +18,35 @@ def is_safe(report: list[int]) -> bool:
             decreasing = False
         if not increasing and not decreasing:
             print("not monotone")
-            return False
-    return True
+            return i
+    return None
 
 
 def part_one(input: Input) -> int | None:
     reports = [list(map(int, line.split())) for line in input.lines()]
-    return sum([is_safe(report) for report in reports])
+    return sum([has_problem(report) is None for report in reports])
 
 
 def part_two(input: Input) -> int | None:
-    pass
+    reports = [list(map(int, line.split())) for line in input.lines()]
+    safe = 0
+    for r, report in enumerate(reports):
+        idx = has_problem(report)
+        if idx is None:
+            safe += 1
+            print(f"report {r+1} safe")
+        else:
+            print(f"report {r+1} not safe, checking tolerance")
+            for i in [idx - 1, idx, idx + 1]:
+                if i < 0:
+                    continue
+                new_report = report[:i] + report[i + 1 :]
+                if has_problem(new_report) is None:
+                    safe += 1
+                    print(f"  removing index {i} makes report safe")
+                    break
+            print("  report not safe")
+    return safe
 
 
 if __name__ == "__main__":
