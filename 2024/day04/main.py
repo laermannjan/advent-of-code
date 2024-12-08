bae8c646f77ba9f84789ffcdc6d6f5ae3bd7834e
Jan Laermann <git@flabber.mozmail.com>
2024-12-08 13:30:19 +0100

    python: 2024/4

--- 2024/day04/main.py
+++ 2024/day04/main.py
@@ -8,7 +8,7 @@ def part_one(input: Input) -> int | None:
     grid = list(input.lines())
     cnt = 0
     for (row, col), letter in input.coords():
-        if grid[row][col] != "X":
+        if letter != "X":
             continue
         for dr, dc in product([-1, 0, 1], repeat=2):
             if dc == dr == 0:
@@ -17,8 +17,8 @@ def part_one(input: Input) -> int | None:
                 0 <= row + 3 * dr < len(grid[row]) and 0 <= col + 3 * dc < len(grid)
             ):
                 continue
-            for i, letter in enumerate("MAS", start=1):
-                if grid[row + i * dr][col + i * dc] != letter:
+            for i, l in enumerate("MAS", start=1):
+                if grid[row + i * dr][col + i * dc] != l:
                     break
             else:
                 cnt += 1
@@ -26,7 +26,22 @@ def part_one(input: Input) -> int | None:
 
 
 def part_two(input: Input) -> int | None:
-    pass
+    grid = list(input.lines())
+    cnt = 0
+    for (row, col), letter in input.coords():
+        if row == 0 or row == len(grid) - 1 or col == 0 or col == len(grid[0]) - 1:
+            continue
+
+        if letter != "A":
+            continue
+
+        surround = "".join(
+            grid[row + dr][col + dc]
+            for (dr, dc) in [(-1, -1), (-1, 1), (1, 1), (1, -1)]
+        )
+        if surround in ["MMSS", "SMMS", "SSMM", "MSSM"]:
+            cnt += 1
+    return cnt
 
 
 if __name__ == "__main__":
@@ -35,5 +50,5 @@ if __name__ == "__main__":
     ) == 18, f"Part one failed, {p1=}"
     assert (
         p2 := part_two(Input.from_file_relpath("example.txt"))
-    ) == None, f"Part two failed, {p2=}"
+    ) == 9, f"Part two failed, {p2=}"
     Day(part_one, part_two).run()
