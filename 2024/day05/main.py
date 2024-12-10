bffe84bf1e70b1ec89e9662b7f29cbf42f76843f
Jan Laermann <git@flabber.mozmail.com>
2024-12-10 20:08:24 +0100

    python: 2024/5

--- 2024/day05/main.py
+++ 2024/day05/main.py
@@ -1,36 +1,60 @@
+from functools import cmp_to_key
 from itertools import combinations
 
 from utils.execute import Day
 from utils.input import Input
 
 
-def is_ordered(update: list[int], rules: dict[tuple[int, int], bool]) -> bool:
-    for i, j in combinations(update, 2):
-        if not rules.get((i, j)):
-            return False
-    return True
-
-
 def part_one(input: Input) -> int | None:
-    rules, updates = (s for s in input.sections())
+    rules_, updates = (s for s in input.sections())
 
-    rule_cache = {}
-    for rule in rules:
+    rules = {}
+    for rule in rules_:
         x, y = map(int, rule.split("|"))
-        rule_cache[(x, y)] = True
-        rule_cache[(y, x)] = False
+        rules[(x, y)] = True
+        rules[(y, x)] = False
+
+    def is_ordered(update: list[int]) -> bool:
+        for i, j in combinations(update, 2):
+            if not rules.get((i, j)):
+                return False
+        return True
 
     result = 0
     for update in updates:
         update = list(map(int, update.split(",")))
-        if is_ordered(update, rule_cache):
+        if is_ordered(update):
             result += update[len(update) // 2]
 
     return result
 
 
 def part_two(input: Input) -> int | None:
-    pass
+    rules_, updates = (s for s in input.sections())
+
+    rules = {}
+    for rule in rules_:
+        x, y = map(int, rule.split("|"))
+        rules[(x, y)] = -1
+        rules[(y, x)] = 1
+
+    def is_ordered(update: list[int]) -> bool:
+        for i, j in combinations(update, 2):
+            if rules.get((i, j)) == 1:
+                return False
+        return True
+
+    result = 0
+
+    def cmp(x, y):
+        return rules.get((x, y), 0)
+
+    for update in updates:
+        update = list(map(int, update.split(",")))
+        if not is_ordered(update):
+            update.sort(key=cmp_to_key(cmp))
+            result += update[len(update) // 2]
+    return result
 
 
 if __name__ == "__main__":
@@ -39,5 +63,5 @@ if __name__ == "__main__":
     ) == 143, f"Part one failed, {p1=}"
     assert (
         p2 := part_two(Input.from_file_relpath("example.txt"))
-    ) == None, f"Part two failed, {p2=}"
+    ) == 123, f"Part two failed, {p2=}"
     Day(part_one, part_two).run()
