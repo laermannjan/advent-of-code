fcb52898c5f589bcbcd3eada33e54c2766fb3618
Jan Laermann <git@flabber.mozmail.com>
2024-12-13 00:57:53 +0100

    python: 2024/6

--- 2024/day06/main.py
+++ 2024/day06/main.py
@@ -3,58 +3,102 @@ from utils.input import Input
 
 
 def part_one(input: Input) -> int | None:
-    moves = {"^": (-1, 0), ">": (0, 1), "v": (1, 0), "<": (0, -1)}
     grid = list(input.lines())
 
     def in_bounds(row, col):
         return 0 <= row < len(grid) and 0 <= col < len(grid[0])
 
-    def next_pos(row, col, direction):
-        return row + moves[direction][0], col + moves[direction][1]
-
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            ele = grid[row][col]
-            if ele in ("^", "v", "<", ">"):
-                guard_row, guard_col = (row, col)
-                direction = ele
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if grid[r][c] == "^":
+                break
+        else:
+            continue
+        break
     else:
         RuntimeError("guard not found")
 
-    positions = {(guard_row, guard_col)}
+    positions = set()
+    dr, dc = -1, 0
 
     while True:
-        next_row_maybe, next_col_maybe = next_pos(guard_row, guard_col, direction)
-        print(f"looking at ({next_row_maybe}, {next_col_maybe})")
+        positions.add((r, c))
 
-        if not in_bounds(next_row_maybe, next_col_maybe):
-            print(f"({next_row_maybe}, {next_col_maybe}) is out of bounds")
+        nr, nc = r + dr, c + dc
+        print(f"looking at ({nr}, {nc})")
+
+        if not in_bounds(nr, nc):
+            print(f"({nr}, {nc}) is out of bounds")
             break
 
-        if grid[next_row_maybe][next_col_maybe] == "#":
-            print(f"({next_row_maybe}, {next_col_maybe}) is #")
-            if direction == "^":
-                print("turning >")
-                direction = ">"
-            elif direction == ">":
-                print("turning v")
-                direction = "v"
-            elif direction == "v":
-                print("turning <")
-                direction = "<"
-            elif direction == "<":
-                print("turning ^")
-                direction = "^"
+        if grid[nr][nc] == "#":
+            print(f"({nr}, {nc}) is #")
+            dr, dc = dc, -dr
             continue
-        guard_row, guard_col = next_row_maybe, next_col_maybe
-        positions.add((guard_row, guard_col))
+
+        r = r + dr
+        c = c + dc
+
         # sleep(0.5)
 
     return len(positions)
 
 
 def part_two(input: Input) -> int | None:
-    pass
+    grid = [list(line) for line in input.lines()]
+
+    def in_bounds(row, col):
+        return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if grid[r][c] == "^":
+                break
+        else:
+            continue
+        break
+    else:
+        RuntimeError("guard not found")
+
+    def walk(grid, r, c):
+        positions = set()
+        dr, dc = -1, 0
+
+        while True:
+            positions.add((r, c, dr, dc))
+
+            nr, nc = r + dr, c + dc
+            # print(f"looking at ({nr}, {nc})")
+
+            if not in_bounds(nr, nc):
+                # print(f"({nr}, {nc}) is out of bounds")
+                return positions, False
+
+            if grid[nr][nc] == "#":
+                # print(f"({nr}, {nc}) is #")
+                dr, dc = dc, -dr
+            else:
+                r = r + dr
+                c = c + dc
+
+            if (r, c, dr, dc) in positions:
+                return positions, True
+
+    # get the guards path
+    positions, _ = walk(grid, r, c)
+
+    # place obstactle at every step of the guard's original path and see if it loops
+    loops = set()
+    for rr, cc, _, _ in positions:
+        if grid[rr][cc] == ".":
+            grid[rr][cc] = "#"
+            _, looped = walk(grid, r, c)
+            if looped:
+                print(f"found loop with obstactle at ({rr},{cc})")
+                loops.add((rr, cc))
+            grid[rr][cc] = "."
+
+    return len(loops)
 
 
 if __name__ == "__main__":
@@ -63,5 +107,5 @@ if __name__ == "__main__":
     ) == 41, f"Part one failed, {p1=}"
     assert (
         p2 := part_two(Input.from_file_relpath("example.txt"))
-    ) == None, f"Part two failed, {p2=}"
+    ) == 6, f"Part two failed, {p2=}"
     Day(part_one, part_two).run()
