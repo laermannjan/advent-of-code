6f43d2c96044b4559053b9444eaf84252bcacc74
Jan Laermann <git@flabber.mozmail.com>
2024-12-20 14:43:32 +0100

    update script and templates

--- template.py
+++ template.py
@@ -1,20 +1,4 @@
-from utils.execute import Day
-from utils.input import Input
+import sys
 
-
-def part_one(input: Input) -> int | None:
-    pass
-
-
-def part_two(input: Input) -> int | None:
-    pass
-
-
-if __name__ == "__main__":
-    assert (
-        p1 := part_one(Input.from_file_relpath("example.txt"))
-    ) == None, f"Part one failed, {p1=}"
-    assert (
-        p2 := part_two(Input.from_file_relpath("example.txt"))
-    ) == None, f"Part two failed, {p2=}"
-    Day(part_one, part_two).run()
+# print final result to stderr
+print("not implemented", file=sys.stderr)
--- template.rs
+++ template.rs
@@ -1,35 +1,7 @@
-use utils::Day;
+use std::io::{self, Write};
 
-pub fn part_one(input: &str) -> Option<i32> {
-    None
-}
+fn main() {
 
-pub fn part_two(input: &str) -> Option<i32> {
-    None
-}
-
-const DAY: Day = Day {
-    loc: file!(),
-    part_one,
-    part_two,
-};
-pub fn main() {
-    let _ = DAY.run();
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let input = include_str!("./example.txt");
-        assert_eq!(part_one(input), Some(420));
-    }
-
-    #[test]
-    fn test_part_two() {
-        let input = include_str!("./example.txt");
-        assert_eq!(part_two(input), Some(420));
-    }
+    // print final result to stderr
+    writeln!(io::stderr(), "not implemented").unwrap();
 }
