318e43cc0990341547223188c6caf43a4dbb762d
Jan Laermann <git@flabber.mozmail.com>
2024-12-23 17:31:33 +0100

    python: I actually solved 7 not 8

--- 2024/day07/a.py
+++ 2024/day07/a.py
@@ -1,4 +1,23 @@
 import sys
+from itertools import product
 
-# print final result to stderr
-print("not implemented", file=sys.stderr)
+from utils import input
+
+total = 0
+for line in input.lines():
+    res = int(line.split(": ")[0])
+    nums = [int(d) for d in line.split(": ")[1].split(" ")]
+
+    combos = list(product([int.__add__, int.__mul__], repeat=len(nums) - 1))
+
+    print(f"{res=}, {nums=}, {len(nums)=}, {len(combos)=}")
+    for operators in combos:
+        result = nums[0]
+        for i, op in enumerate(operators):
+            result = op(result, nums[i + 1])
+        print(f"  {result=}")
+        if result == res:
+            total += res
+            print("    matched")
+            break
+print(total, file=sys.stderr)
