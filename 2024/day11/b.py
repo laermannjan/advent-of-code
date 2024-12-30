a555129bb387c7b2197c77b3515629fb8ea7d579
Jan Laermann <git@flabber.mozmail.com>
2024-12-30 15:55:04 +0100

    python: 2024/11 boost

--- 2024/day11/b.py
+++ 2024/day11/b.py
@@ -5,38 +5,36 @@ from utils import input
 
 
 @cache
-def blink(stone: str, blinks: int):
-    next_stones: list[str | list[str]]
+def blink(stone: int, blinks: int):
+    # print(f"{stone=}, {blinks=}")
+    if blinks < 1:
+        return [stone]
+    string = str(stone)
+    length = len(string)
 
-    if stone == "0":
-        next_stones = ["1"]
-    elif len(stone) % 2 == 0:
-        next_stones = [stone[: len(stone) // 2], str(int(stone[len(stone) // 2 :]))]
+    if stone == 0:
+        return blink(1, blinks - 1)
+    elif length % 2 == 0:
+        return [
+            *blink(int(string[: length // 2]), blinks - 1),
+            *blink(int(string[length // 2 :]), blinks - 1),
+        ]
     else:
-        next_stones = [str(int(stone) * 2024)]
-
-    print(f"computing {stone=} - {blinks=} - {next_stones=}")
-    blinked_stones = []
-    for next_stone in next_stones:
-        blinked_stones.extend(
-            blink(next_stone, blinks - 1) if blinks > 1 else [next_stone]
-        )
-
-    print(f"returning {stone=} - {blinks=} - {blinked_stones=}")
-    return blinked_stones
+        return blink(stone * 2024, blinks - 1)
 
 
 def main():
-    BLINKS = 6
-    stones = input.lines()[0].split(" ")
+    BLINKS = 75
+
+    stones = [int(x) for x in input.lines()[0].split()]
 
     print(f"{stones=}")
     final_stones = []
-    for s, stone in enumerate(stones):
+    for stone in stones:
         print(f"OG STONE {stone=}")
         final_stones.extend(blink(stone, BLINKS))
+        # print(f"{final_stones=}")
 
-    print(f"{final_stones=}")
     print(f"{len(final_stones)} - after {BLINKS} blinks", file=sys.stderr)
 
 
