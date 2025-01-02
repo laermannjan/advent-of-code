0feb7a14802eff61a344354bce2b534b836aef64
Jan Laermann <git@flabber.mozmail.com>
2025-01-02 09:51:40 +0100

    python: 2024/15 cleanup

--- 2024/day15/b.py
+++ 2024/day15/b.py
@@ -34,25 +34,13 @@ def main():
 
     for inst in instructions:
         print("-" * 20)
-        if inst == "<":
-            dc = -1
+        if inst in "<>":
+            # left/right still can only happen in a single row
+            dc = 1 if inst == ">" else -1
 
             boxes = []
             nr, nc = rr, rc + dc
-            while house[nr][nc] == "]":
-                boxes.append((nr, nc))
-                nc += dc + dc
-
-            if house[nr][nc] != "#":
-                house[nr].pop(nc)
-                house[rr].insert(rc, ".")
-                rr, rc = rr, rc + dc
-
-        elif inst == ">":
-            dc = 1
-            boxes = []
-            nr, nc = rr, rc + dc
-            while house[nr][nc] == "[":
+            while house[nr][nc] in "[]":
                 boxes.append((nr, nc))
                 nc += dc + dc
 
@@ -63,36 +51,38 @@ def main():
 
         elif inst in ["^", "v"]:
             dr = -1 if inst == "^" else 1
-            if house[rr + dr][rc] == ".":
-                house[rr + dr][rc] = "@"
-                house[rr][rc] = "."
-                rr, rc = rr + dr, rc
 
-            elif house[rr + dr][rc] == "#":
-                pass
-            else:
+            # look up/down, if there is a box half, add it and the other part to the stack
+            # for each box-half on the stack see if it's a left or a right half of a box
+            # if it is a box-half, add it and its counterpart to the box set
+            # also, add the two coordinates above/below it to the stack
+            # i.e. a "detected" box will always add the two coords directly above/below it
+            # and because a box half automatically adds the counterpart half, we can expand
+            if house[rr + dr][rc] not in ".#":
                 boxes = set()
-                stack = [(rr + dr, rc)]
+                stack = {(rr + dr, rc)}
                 if house[rr + dr][rc] == "[":
-                    stack.append((rr + dr, rc + 1))
+                    stack.add((rr + dr, rc + 1))
                 elif house[rr + dr][rc] == "]":
-                    stack.append((rr + dr, rc - 1))
+                    stack.add((rr + dr, rc - 1))
 
                 while stack:
                     r, c = stack.pop()
                     if house[r][c] == "[":
                         boxes.add((r, c))
                         boxes.add((r, c + 1))
-                        stack.append((r + dr, c))
-                        stack.append((r + dr, c + 1))
+                        stack.add((r + dr, c))
+                        stack.add((r + dr, c + 1))
                     elif house[r][c] == "]":
                         boxes.add((r, c))
                         boxes.add((r, c - 1))
-                        stack.append((r + dr, c - 1))
-                        stack.append((r + dr, c))
+                        stack.add((r + dr, c - 1))
+                        stack.add((r + dr, c))
                 print(boxes)
 
                 for box in boxes:
+                    # we need to check for every box if there would be a collision
+                    # because we could have "gaps", e.g. a U-shaped pile of boxes infront of us
                     r, c = box
                     if house[r + dr][c] == "#":
                         break
@@ -105,10 +95,10 @@ def main():
                         new_house[r + dr][c] = house[r][c]
                         new_house[r][c] = "."
                     house = new_house
-
-                    house[rr + dr][rc] = "@"
-                    house[rr][rc] = "."
-                    rr, rc = rr + dr, rc
+            if house[rr + dr][rc] == ".":
+                house[rr + dr][rc] = "@"
+                house[rr][rc] = "."
+                rr, rc = rr + dr, rc
 
         else:
             raise ValueError("invalid instruction", inst)
